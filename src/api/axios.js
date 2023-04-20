6688845749cecdbdecb063cc0764b0015e23d39b
raymccarthy5 <54141956+raymccarthy5@users.noreply.github.com>
2023-04-20 17:50:54 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+//const BASE_URL = 'http://localhost:8080/api/v1';
 
-//const BASE_URL = 'https://library-management-backend.herokuapp.com/api/v1';
+const BASE_URL = 'https://library-management-backend.herokuapp.com/api/v1';
 
 export default axios.create({
   baseURL: BASE_URL
