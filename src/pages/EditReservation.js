0e413e92af1d4f6cd919eb3d6bf0c1a561543ff6
Raymond McCarthy <x00179223@mytudublin.ie>
2023-04-21 17:04:48 +0100

    fix logic for manage reservation buttons

--- src/pages/EditReservation.js
+++ src/pages/EditReservation.js
@@ -168,23 +168,52 @@ const EditReservation = () => {
                 <p>Returned: {reservation?.checkedOutAt? (reservation?.returned ? 'Yes' : 'No') : 'N/A'}</p>
               </Card.Text>
                 { reservation.checkedOutAt ? (
-                <button onClick={() => handleShowExtendModal(reservation.id)}>Extend</button>) : <button
-                disabled={!reservation.checkedOutAt}
-                className={!reservation.checkedOutAt ? "btn btn-secondary" : "button"}
-                >Extend</button>
-                }
+                  <button 
+                    onClick={() => handleShowExtendModal(reservation.id)}
+                    disabled={reservation.returned}
+                    className={reservation.returned ? "btn btn-secondary" : "button"}
+                    >
+                      Extend
+                    </button>
+                    ) : ( 
+                    <button
+                      disabled={!reservation.checkedOutAt || reservation.returned}
+                      className={!reservation.checkedOutAt ? "btn btn-secondary" : "button"}
+                    >
+                      Extend
+                    </button>
+                    )}
+
+
                 { !reservation.checkedOutAt ? (
                 <button onClick={() => handleShowCancelModal(reservation.id)}>Cancel</button>) : <button
                 disabled={reservation.checkedOutAt}
                 className={reservation.checkedOutAt ? "btn btn-secondary" : "button"}
                 >Cancel</button>
                 }
+
+
+
                 { reservation.checkedOutAt ? (
-                <button onClick={() => handleShowCheckinModal(reservation.id)}>Check-in</button>) : <button
-                disabled={!reservation.checkedOutAt}
-                className={!reservation.checkedOutAt ? "btn btn-secondary" : "button"}
-                >Check-in</button>
-                }
+                  <button
+                    onClick={() => handleShowCheckinModal(reservation.id)}
+                    disabled={reservation.returned}
+                    className={reservation.returned ? "btn btn-secondary" : "button"}
+                  >
+                    Check-in
+                  </button>
+                ) : (
+                  <button
+                    disabled={!reservation.checkedOutAt }
+                    className={!reservation.checkedOutAt ? "btn btn-secondary" : "button"}
+                  >
+                    Check-in
+                  </button>
+                )}
+
+
+
+
                 { !reservation.checkedOutAt ? (
                 <button onClick={() => handleShowCheckoutModal(reservation.id)}>Check-out</button>) : <button
                 disabled={reservation.checkedOutAt}
